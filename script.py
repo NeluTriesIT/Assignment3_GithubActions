04d9d5a44e46de3f4b4c7623e079a605db46d95d
NeluTriesIT <nelutriesit@gmail.com>
2023-12-07 10:42:23 -0500

    add spaces for linter test

--- script.py
+++ script.py
@@ -8,7 +8,13 @@ def frequency(numbers):
     counts = Counter(numbers)
     return counts.most_common(1)[0][0]
 
+
+
+
 if __name__ == "__main__":
+
+
+    
     numbers = [int(arg) for arg in sys.argv[1:]]
     max = frequency(numbers)
     print(max)

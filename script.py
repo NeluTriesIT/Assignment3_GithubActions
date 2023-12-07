0913fa0e502a1d88860a7d927a6e6aacb0887a3d
NeluTriesIT <122745181+NeluTriesIT@users.noreply.github.com>
2023-12-07 17:53:23 +0200

    Update script.py after linter

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import sys
-import os
 from collections import Counter
+
 
 def frequency(numbers):
     if not numbers:
@@ -9,13 +9,7 @@ def frequency(numbers):
     return counts.most_common(1)[0][0]
 
 
-
-
 if __name__ == "__main__":
-
-
-    
     numbers = [int(arg) for arg in sys.argv[1:]]
     max = frequency(numbers)
-    print(max)
-    
+    print(max) 
